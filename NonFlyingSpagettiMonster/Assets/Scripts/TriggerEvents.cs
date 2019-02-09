7da5b18442ce892fd96ceaa1bfbb6d36c5deb0c5
gareau <thilo@asablackbox.com>
2019-02-09 18:29:25 +0900

    The fianal game

--- NonFlyingSpagettiMonster/Assets/Scripts/TriggerEvents.cs
+++ NonFlyingSpagettiMonster/Assets/Scripts/TriggerEvents.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;   // Add UnityEngine.Events library to use UnityEvents
@@ -7,6 +7,7 @@ public class TriggerEvents : MonoBehaviour
 {
     public string tagName = "Button";   // Use tag name filter out only the objects that we want
     public UnityEvent onTriggerEvent;   // Event to tell GameObjects listening that a Collider event was triggered
+private int count=0;
 
     /// <summary>
     /// This method is called when an object enter a Collider trigger
@@ -15,10 +16,11 @@ public class TriggerEvents : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         // Check that the object collided with is a button
-        if (other.gameObject.tag == tagName)
+        if (other.gameObject.tag == tagName && count==0)
         {
             Debug.Log(other.gameObject.tag + " triggered me!");
             // Let all listeners know that the button has been triggered
+		count=1;
             onTriggerEvent.Invoke();
         }
         
